07a322af64f27187dfac2a250ad5251324ad1c35
Marek Michalik <marekmichalik43@gmail.com>
2018-02-10 15:57:40 +0100

    Add downsizing

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/color"
 	_ "image/jpeg"
 	"os"
-	"image"
-	"fmt"
+
 	"github.com/bugra/kmeans"
-	"image/color"
 )
 
 func main() {
@@ -17,17 +18,17 @@ func main() {
 		"example_images/Fotolia_45549559_320_480.jpg",
 		"example_images/giant-panda-shutterstock_86500690.jpg",
 		"example_images/mac_os_x_retina_zebras-wallpaper-1920x1080.jpg",
-		}
+	}
 
 	fmt.Println("<div>")
-	for _, file := range files{
+	for _, file := range files {
 		imageFile, _ := os.Open(file)
 		defer imageFile.Close()
 
 		image, _, _ := image.Decode(imageFile)
 		colors := extractColors(image, 5)
 
-		fmt.Println("<img src=\""+file+"\" width=\"200\"><br>")
+		fmt.Println("<img src=\"" + file + "\" width=\"200\"><br>")
 		for _, c := range colors {
 			printColor(c)
 
@@ -39,20 +40,23 @@ func main() {
 
 func printColor(c color.Color) {
 	r, g, b, _ := c.RGBA()
-	fmt.Print("<div style=\"background-color:rgb(",r>>8,",",g>>8,",",b>>8,");display:inline-block;width:40px;height:40px;margin-right:-5px;\"></div>\n")
+	fmt.Print("<div style=\"background-color:rgb(", r>>8, ",", g>>8, ",", b>>8, ");display:inline-block;width:40px;height:40px;margin-right:-5px;\"></div>\n")
 }
 
 func extractColors(image image.Image, colorsCount int) []color.Color {
 	width := image.Bounds().Max.X
 	height := image.Bounds().Max.Y
 
+	// calculate downsizing ratio
+	step := width/256 + 1
+
 	// load image's pixels into [][]float64
 	colorData := [][]float64{}
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
+	for x := 0; x < width; x += step {
+		for y := 0; y < height; y += step {
 			color := image.At(x, y)
 			r, g, b, _ := color.RGBA()
-			colorData = append(colorData, []float64{float64(r>>8), float64(g>>8), float64(b>>8)})
+			colorData = append(colorData, []float64{float64(r >> 8), float64(g >> 8), float64(b >> 8)})
 		}
 	}
 
