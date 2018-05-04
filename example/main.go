d401144d76f6820fdf071270b1ed1ff9d30a5eeb
Marek Michalik <marekmichalik43@gmail.com>
2018-05-04 19:44:04 +0200

    Restore original example

--- example/main.go
+++ example/main.go
@@ -1,31 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"image"
-	"image/color"
 	_ "image/jpeg"
-	"image/png"
-	"io"
 	"os"
 
 	"github.com/marekm4/color-extractor"
 )
 
 func main() {
-	image, _, _ := image.Decode(os.Stdin)
-	colors := color_extractor.ExtractColors(image)
-	createPalette(os.Stdout, colors)
-}
+	file := "Fotolia_45549559_320_480.jpg"
+	imageFile, _ := os.Open(file)
+	defer imageFile.Close()
 
-func createPalette(w io.Writer, colors []color.Color) {
-	squareSize := 40
-	image := image.NewRGBA(image.Rect(0, 0, len(colors)*squareSize, squareSize))
-	for i, color := range colors {
-		for j := 0; j < squareSize; j++ {
-			for k := 0; k < squareSize; k++ {
-				image.Set(i*squareSize+j, k, color)
-			}
-		}
-	}
-	png.Encode(w, image)
+	image, _, _ := image.Decode(imageFile)
+	colors := color_extractor.ExtractColors(image)
+
+	fmt.Println(colors)
 }
